edb8eda18dbb245b99969f7dc4de6ece6aa85f8b
OttoFidelis <131968694+OttoFidelis@users.noreply.github.com>
2025-04-15 12:35:18 +0000

    Tirei uma redundância na estrutura das classes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
-import methods.Executar;
+import java.util.Random;
+import java.util.Scanner;
+
+import enums.Palavra;
+import methods.Start;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Executar.executar();
+        Random palavras = new Random();
+        int tentativas = 6;
+        int total = Palavra.totalPalavras();
+        String palavra = Palavra.fromValor(palavras.nextInt(total) + 1).toString().toLowerCase();
+        String palavraGameOver = palavra;
+        char[] palavraFormada = new char[palavra.length()];
+        boolean temLetra = true;
+
+        for (int i = 0; i < palavraFormada.length; i++) {
+            palavraFormada[i] = '_';
+        }
+        
+        String dicaInicial = new String(palavraFormada);
+        Scanner tcd = new Scanner(System.in);
+
+        Start.start(dicaInicial, tentativas, temLetra, palavra, palavraFormada, palavraGameOver, tcd);
     }
 }
